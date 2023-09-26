93826366a2504275a75e360dc9eae824b0048000
Asif-36 <130299330+Asif-36@users.noreply.github.com>
2023-09-26 22:18:45 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return const MaterialApp (home:HomeActivity());
+
+  }
+}
+
+
+  class HomeActivity extends StatelessWidget{
+  const HomeActivity({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(title: Text("My App1"),),
+      body: Text("Hello"),
+
+
+    );
+  }
+
+
+  }
+
+
